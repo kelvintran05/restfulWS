9b5d5f0c690c9dbdc5579aade1441a79f7551fd8
Tri Tran <triit95@gmail.com>
2019-01-14 14:11:00 +0700

    first commit

---
+++ src/test/java/com/tctri/rest/webservices/restfulWS/RestfulWsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.tctri.rest.webservices.restfulWS;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestfulWsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
