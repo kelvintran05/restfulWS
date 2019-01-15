7cc058ff3b24f51515df34f6852368967ef4fd36
Tri Tran <triit95@gmail.com>
2019-01-15 16:22:36 +0700

    Customized exception, implementing HATEOAS, configuring auto generation of Swagger.

--- src/main/java/com/tctri/rest/webservices/restfulWS/helloworld/HelloWorldController.java
+++ src/main/java/com/tctri/rest/webservices/restfulWS/helloworld/HelloWorldController.java
@@ -1,12 +1,21 @@
 package com.tctri.rest.webservices.restfulWS.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -21,5 +30,15 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-varible/{name}")
 	public HelloWorldBean helloWorldVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+	
+//	@GetMapping(path = "/hello-world-internationalized")
+//	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
+//		return messageSource.getMessage("good.morning.message", null, locale);
+//	}
+	
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
