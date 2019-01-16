32bd304b11da0e0760d2566c8773972eac0b2f9c
Tri Tran <triit95@gmail.com>
2019-01-16 15:59:21 +0700

    commited

--- src/main/java/com/tctri/rest/webservices/restfulWS/user/User.java
+++ src/main/java/com/tctri/rest/webservices/restfulWS/user/User.java
@@ -2,17 +2,29 @@ package com.tctri.rest.webservices.restfulWS.user;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="All details about the user")
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
 	private  Integer id;
 	
 	@Size(min= 2, message= "Name should have atleast 2 characters")
+	@ApiModelProperty(notes="Name should have atleast 2 characters")
 	private String name;
 	
 	@Past
+	@ApiModelProperty(notes="Birth date should be in the past")
 	private Date birthDate;
 
 	protected User() {
