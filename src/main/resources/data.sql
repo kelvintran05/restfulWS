28f0c0b950fb83b3e6ce2cb87401c21433e4867d
Tri Tran <triit95@gmail.com>
2019-01-17 17:08:57 +0700

    commited

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
-insert into user values(1, sysdate(), 'AB');
-insert into user values(2, sysdate(), 'Jack');
-insert into user values(3, sysdate(), 'Jill');
+insert into user values(10001, sysdate(), 'AB');
+insert into user values(10002, sysdate(), 'Jack');
+insert into user values(10003, sysdate(), 'Jill');
+insert into post values(11001, 'My first', 10001);
+insert into post values(11002, 'My second', 10001);
